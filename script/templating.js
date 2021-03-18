f737875e66b295b16b1a1adf0cd53430fddab6be
Magnus <magnus.duerr@assaabloy.com>
2021-03-18 22:41:04 +0100

    Working on finalizing SM. Added list change log as separate file

--- script/templating.js
+++ script/templating.js
@@ -24,15 +24,10 @@ var eaTemplating = {
             eaTemplating.templates['units'] = Handlebars.compile(template);
         });
 
-        $.get("templates/about.html").done(function (template) {
-            eaTemplating.templates['about'] = Handlebars.compile(template);
-        });
-
         $.get("templates/menu.html").done(function (template) {
             eaTemplating.templates['menu'] = Handlebars.compile(template);
         });
 
-        // TODO: Deprecated
         $.get("templates/list.html").done(function (template) {
             eaTemplating.templates['list'] = Handlebars.compile(template);
         });
